eff7a6d6fe808b3019a243ed5b30f93efaa8f127
Dima Pershin <p3rschin.dmit@yandex.ru>
2023-04-21 17:40:12 +0500

    fixed: ревью.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,20 @@
 const popupElement = document.querySelector('.popup');
 const popupEditButtonElement = document.querySelector('.profile__edit-profile');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupForm = popupElement.querySelector('.popup__content');
 let nameInputElement = popupElement.querySelector('.popup__input_info_name');
 let infoInputElement = popupElement.querySelector('.popup__input_info_job');
 let nameElement = document.querySelector('.profile__title');
 let infoElement = document.querySelector('.profile__subtitle');
 
 function openPopup() {
-  popupElement.classList.add('popup_open');
+  popupElement.classList.add('popup_opened');
   nameInputElement.value = nameElement.textContent;
   infoInputElement.value = infoElement.textContent;
 }
 
 function closePopup() {
-  popupElement.classList.remove('popup_open');
+  popupElement.classList.remove('popup_opened');
 }
 
 function handleFormSubmit(evt) {
@@ -25,4 +26,4 @@ function handleFormSubmit(evt) {
 
 popupEditButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('submit', handleFormSubmit);
+popupForm.addEventListener('submit', handleFormSubmit);
